00654f96721bd279308f7fe96e6599775fabc502
LuQuan Intrepid <LuQuan@LuQuans-MacBook-Pro.local>
2014-11-25 15:20:34 -0500

    Initial commit for webservice

---
+++ ImaginariumDemo/ImaginariumWebService.h
@@ -0,0 +1,15 @@
+//
+//  ImaginariumWebService.h
+//  ImaginariumDemo
+//
+//  Created by LuQuan Intrepid on 11/25/14.
+//  Copyright (c) 2014 LuQuan Intrepid. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface ImaginariumWebService : NSObject
+
+
+@end
---
+++ ImaginariumDemo/ImaginariumWebService.m
@@ -0,0 +1,13 @@
+//
+//  ImaginariumWebService.m
+//  ImaginariumDemo
+//
+//  Created by LuQuan Intrepid on 11/25/14.
+//  Copyright (c) 2014 LuQuan Intrepid. All rights reserved.
+//
+
+#import "ImaginariumWebService.h"
+
+@implementation ImaginariumWebService
+
+@end
